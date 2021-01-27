ffa1e6e54b78677121bfad2c13670fb59a03f9cd
zeeshan <itexpert120@gmail.com>
2021-01-27 20:24:36 +0500

    new lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/a5ltechn
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_a5ltechn.mk \
-	$(LOCAL_DIR)/rr_a5ltechn.mk \
 	$(LOCAL_DIR)/lineage_a5ltechn.mk
+	
+COMMON_LUNCH_CHOICES := \
+    lineage_a5ltechn-eng \
+    lineage_a5ltechn-userdebug \
+    lineage_a5ltechn-user
